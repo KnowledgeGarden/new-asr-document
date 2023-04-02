783c213594377b77f7151a2e68490e230f6c0eb0
Jack Park <jackpark@gmail.com>
2023-04-02 14:33:35 -0700

    refactored tocommon kafka producer

--- src/main/java/org/topicquests/newasr/ASRDocumentEnvironment.java
+++ src/main/java/org/topicquests/newasr/ASRDocumentEnvironment.java
@@ -13,8 +13,6 @@ import org.topicquests.newasr.api.IKafkaDispatcher;
 import org.topicquests.newasr.impl.ASRBaseEnvironment;
 import org.topicquests.newasr.impl.DocumentListener;
 import org.topicquests.newasr.kafka.KafkaHandler;
-import org.topicquests.newasr.kafka.DocumentProducer;
-import org.topicquests.support.RootEnvironment;
 import org.topicquests.support.config.Configurator;
 
 /**
@@ -26,22 +24,19 @@ public class ASRDocumentEnvironment extends ASRBaseEnvironment {
 	private KafkaHandler documentConsumer;
 	private Map<String,Object>kafkaProps;
 	private IKafkaDispatcher documentListener;
-	private DocumentProducer sentenceProducer;
 	private DocumentEngine docEngine;
 	
-	public static final String AGENT_GROUP = "Document";
 
 	/**
 	 * @param configPath
 	 * @param logConfigPath
 	 */
 	public ASRDocumentEnvironment() {
-		super("asr-document-config.xml", null, "logger.properties");
+		super("asr-document-config.xml", "logger.properties");
 		kafkaProps = Configurator.getProperties("kafka-topics.xml");
 		documentListener = new DocumentListener(this);
 		String cTopic = (String)kafkaProps.get("DocumentConsumerTopic");
 		documentConsumer = new KafkaHandler(this, (IMessageConsumerListener)documentListener, cTopic, AGENT_GROUP);
-		sentenceProducer = new DocumentProducer(this, AGENT_GROUP);
 		model = null; //TODO
 		docEngine = new DocumentEngine(this);
 		
